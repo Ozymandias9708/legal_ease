8d3bb133cd406a174a9e1991f832fcf37ae030cf
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-10-26 20:36:01 +0530

    Adding More Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './components/Register';
+import UserType from './components/UserType';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+        <>
+        <Router>
+        <Navbar />
+        <Routes>
+        <Route exact path='/' element={<UserType />}/>
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/register' element={<Register />}/>
+        </Routes>
+        </Router>
+        </>
+    </>
   );
 }
 
