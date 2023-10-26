614b1fa153337888404e93978029a2e89d14ebf7
Shubham Kumar <shubham9905374230@gmail.com>
2023-10-26 21:35:19 +0530

    s

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
+
+  const handleClick1=() => {
+    
+  }
+  const handleClick2=() => {
+    
+  }
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <button onClick={handleClick1}>I am Consumer</button>
+            <button onClick={handleClick2}>I am Service Provider</button>
+          </Route>
+          <Route path="/consumerHome">
+            
+          </Route>
+          <Route path="/providerHome">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
