76be230c470c4514d6c63d9fb1169d9892ec10cf
Shubham Kumar <shubham9905374230@gmail.com>
2023-10-26 22:19:15 +0530

    Separitng

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
+import Navbar from './ServiceComponents/Navbar';
+import ClientHome from './ClientComponents/ClientHome';
+import ServiceHome from './ServiceComponents/ServiceHome';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
-
+  const history = useHistory()
   const handleClick1=() => {
-    
+    history.push("clientHome")
   }
   const handleClick2=() => {
-    
+    history.push("ServiceHome")
   }
   return (
     <div className="App">
-      <Navbar/>
       <Router>
         <Switch>
           <Route exact path="/">
             <button onClick={handleClick1}>I am Consumer</button>
             <button onClick={handleClick2}>I am Service Provider</button>
           </Route>
-          <Route path="/consumerHome">
-            
+          <Route path="/clientHome">
+            <ClientHome/>
           </Route>
-          <Route path="/providerHome">
-
+          <Route path="/ServiceHome">
+            <ServiceHome/>
           </Route>
         </Switch>
       </Router>
