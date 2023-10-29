0b7703ba412a4d236786eab39b3ffe8a160a33f0
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-10-30 00:17:35 +0530

    "changes"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom';
 import UserType from './components/UserType';
@@ -25,39 +24,7 @@ function App() {
         <Route path="/providerCaseslist" element={<CasesTaken />} />
         <Route path="/providerClients" element={<Clients />} />
         <Route path="/providerRequests" element={<Requests />} />
-      </Routes>
-=======
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './ServiceComponents/Navbar';
-import ClientHome from './ClientComponents/ClientHome';
-import ServiceHome from './ServiceComponents/ServiceHome';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-
-function App() {
-  const history = useHistory()
-  const handleClick1=() => { 
-    history.push("clientHome")
-  }
-  const handleClick2=() => {
-    history.push("ServiceHome")
-  }
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <button onClick={handleClick1}>I am Consumer</button>
-            <button onClick={handleClick2}>I am Service Provider</button>
-          </Route>
-          <Route path="/clientHome">
-            <ClientHome/>
-          </Route>
-          <Route path="/ServiceHome">
-            <ServiceHome/>
-          </Route>
-        </Switch>
-      </Router>
->>>>>>> 21cc2e1ee87321abc7ef16faa93662447243e82f
+      </Routes> 
     </div>
   );
 }
