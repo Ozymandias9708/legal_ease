8e6f23f8c5d5d3b24e61fc1ff3caccc6c33ab6af
Shubham Kumar <shubham9905374230@gmail.com>
2023-10-30 08:20:50 +0530

    l

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from './ServiceComponents/Navbar';
+// import Navbar from './ServiceComponents/Navbar';
 import ClientHome from './ClientComponents/ClientHome';
 import ServiceHome from './ServiceComponents/ServiceHome';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from "react-router-dom"
 
 function App() {
   const history = useHistory()
   const handleClick1=() => { 
-    history.push("clientHome")
+    history.push("/clientHome")
   }
   const handleClick2=() => {
-    history.push("ServiceHome")
+    history.push("/ServiceHome")
   }
   return (
     <div className="App">
+      {/* <Navbar/> */}
       <Router>
         <Switch>
           <Route exact path="/">
-            <button onClick={handleClick1}>I am Consumer</button>
-            <button onClick={handleClick2}>I am Service Provider</button>
+            {/* <button onClick={handleClick1}>I am Co nsumer</button> */}
+            {/* <button onClick={handleClick2}>I am Service Provider</button> */}
+            <ClientHome/>
           </Route>
           <Route path="/clientHome">
             <ClientHome/>
--- src/ClientComponents/ClientHome.js
+++ src/ClientComponents/ClientHome.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './ClientNavbar/ClientNavbar'
 
 export default function ClientHome() {
   return (
-    <div>
-      
+    <div className='ClientHome'>
+      <Navbar/>
     </div>
   )
 }
